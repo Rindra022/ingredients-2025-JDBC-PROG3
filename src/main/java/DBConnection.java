9ef66e80952904c264c03b0f4d8a9cd1d2d7edc1
Rindra <hei.rindra.2@gmail.com>
2026-01-29 04:43:43 +0300

    clone repo ingredients-2025

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -6,10 +6,10 @@ public class DBConnection {
 
     public Connection getConnection() {
         try {
-            String jdbcURl = System.getenv("JDBC_URl"); //
+            String jdbcURl = System.getenv("JDBC_URL"); //jdbc:postgresql://localhost:5432/mini_dish_db
             String user = System.getenv("USER"); //mini_dish_db_manager
             String password = System.getenv("PASSWORD"); //123456
-            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/mini_dish_db", "postgres", "postgres");
+            return DriverManager.getConnection(jdbcURl, user, password);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
